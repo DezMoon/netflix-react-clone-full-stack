7712b4626cf6bc063c2a23432e78fceedd8cb704
DezMoon <moongachiku@gmail.com>
2023-04-17 08:56:05 +0200

    Editted get lists

--- api/models/List.js
+++ api/models/List.js
@@ -1,12 +1,13 @@
 const { default: mongoose } = require("mongoose");
 
-const ListSchema = new mongoose.Schema({
-    title:{type:String, requested:true, unique:true},
-    type:{type:String},
-    genre:{type:String},
-    content:{type:Array}
-    },
-    {timestamps:true}
+const ListSchema = new mongoose.Schema(
+  {
+    title: { type: String, requested: true },
+    type: { type: String },
+    genre: { type: String },
+    content: { type: Array },
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("List", ListSchema);
